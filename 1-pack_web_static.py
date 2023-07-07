6250910c6e1871cc4645ff510fc6811a742839dc
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-07 11:40:12 +0300

    remove try catch

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,8 +6,6 @@ execute: fab -f 1-pack_web_static.py do_pack
 
 from datetime import datetime
 from fabric.api import local
-from os.path import isdir
-
 
 
 def do_pack():
@@ -15,12 +13,11 @@ def do_pack():
     making an archive on web_static folder
     """
 
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
         return None
