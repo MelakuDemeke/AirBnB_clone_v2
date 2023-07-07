ae0aa6447178ac98e5374f6b63907dc080b415aa
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-07 11:35:05 +0300

    change date time format

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,11 +13,11 @@ def do_pack():
     making an archive on web_static folder
     """
 
-    time = datetime.now()
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('mkdir -p versions')
-    create = local('tar -cvzf versions/{} web_static'.format(archive))
-    if create is not None:
-        return archive
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
     else:
         return None
