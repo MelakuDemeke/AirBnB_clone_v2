bbae37150e9c2612d7e119d8069f70cbb0a8cf8d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-09 17:12:46 +0300

    add file size print

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,23 +4,24 @@ Fabric script to genereate tgz archive
 execute: fab -f 1-pack_web_static.py do_pack
 """
 
-from datetime import datetime
 from fabric.api import local
-
+from datetime import datetime
+import os
 
 def do_pack():
-    """
-    Create a .tgz archive from the web_static folder
-    """
+    """Generates a .tgz archive from the contents of the web_static folder"""
+    try:
+        if not os.path.exists('versions'):
+            os.makedirs('versions')
+        
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(timestamp)
+        
+        local("tar -czvf {} web_static".format(archive_path))
 
-    time = datetime.now()
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.tgz'
-    local('mkdir -p versions')
-    create = local('tar -czvf versions/{} web_static'.format(archive))
-
-    if create.succeeded:
-        archive_path = 'versions/' + archive
-        local('chmod 664 {}'.format(archive_path))
+        file_size = os.path.getsize(archive_path)
+        print("web_static packed: {} -> {}Bytes".format(archive_path, file_size))
+        
         return archive_path
-    else:
+    except:
         return None
