5dd2a0f3a3ea606fd5d3f150b7abae49c7c99255
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-09 16:45:47 +0300

    fix code to change permission

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,32 @@
-#!/usr/bin/python3
-"""
-Fabric script to genereate tgz archive
-execute: fab -f 1-pack_web_static.py do_pack
-"""
-
 from datetime import datetime
 from fabric.api import local
+import os
 
 
 def do_pack():
     """
-    making an archive on web_static folder
+    Create a .tgz archive from the web_static folder
     """
 
-    time = datetime.now()
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('mkdir -p versions')
-    create = local('tar -cvzf versions/{} web_static'.format(archive))
-    if create is not None:
-        return archive
+    # Create the versions folder if it doesn't exist
+    local("mkdir -p versions")
+
+    # Generate the timestamp for the archive name
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Create the archive using tar and gzip
+    archive_name = "web_static_{}.tgz".format(timestamp)
+    command = "tar -czvf versions/{} web_static".format(archive_name)
+    result = local(command)
+
+    if result.failed:
+        return None
+
+    # Check if the archive was created successfully
+    archive_path = os.path.join("versions", archive_name)
+    if os.path.exists(archive_path):
+        # Set permissions to -rw-rw-r--
+        local("chmod 664 {}".format(archive_path))
+        return archive_path
     else:
         return None
