1dc47301b52eddfc3c98a2b884f06187aec9a425
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-09 17:04:28 +0300

    add chmod 664

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
 """
 
 from datetime import datetime
 from fabric.api import local
-from os.path import isdir
 
 
 def do_pack():
-    """generates a tgz archive"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
+    """
+    Create a .tgz archive from the web_static folder
+    """
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.tgz'
+    local('mkdir -p versions')
+    create = local('tar -czvf versions/{} web_static'.format(archive))
+
+    if create.succeeded:
+        archive_path = 'versions/' + archive
+        local('chmod 664 {}'.format(archive_path))
+        return archive_path
+    else:
         return None
